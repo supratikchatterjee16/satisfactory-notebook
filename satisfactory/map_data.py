2b8e7726d14e279ed297272eefce6129e8dbae74
Supratik Chatterjee <supratikdevm96@gmail.com>
2022-05-14 14:01:41 +0530

    Resource Data Formation fix

--- satisfactory/map_data.py
+++ satisfactory/map_data.py
@@ -1,6 +1,7 @@
 # /home/supratik/.steam/debian-installation/steamapps/compatdata/526870/pfx/drive_c/users/steamuser/AppData/Local/FactoryGame/MapData-Export/
 # %LOCALAPPDATA%\FactoryGame\MapData-Export
 import os
+import time
 import json
 import pandas
 
@@ -35,12 +36,53 @@ class ResourceData:
         with open(self.mapdata_path, 'r') as resources_file:
             resources = json.load(resources_file)
 
-        for key in resources.keys():
-            print(key, len(resources[key]))
+        # for key in resources.keys():
+        #     print(key, len(resources[key]))
+        # Transform all values into the format specified below
+        # name purity x y z
+        nodes_list = []
+        for node in resources['nodes']:
+            res = {}
+            res['name'] = node['item']['name'].lower().replace(' ', '-')
+            res['purity'] = node['purity'][3:]
+            res.update(node['location'])
+            nodes_list.append(res)
+
+        for core in resources['frackingCores']:
+            res = {}
+            res['name'] = core['item']['name'].lower().replace(' ', '-')
+            res['purity'] = None
+            res.update(core['location'])
+            nodes_list.append(res)
+
+        for geyser in resources['geysers']:
+            res = {}
+            res['name'] = geyser['item']['name'].lower().replace(' ', '-')
+            res['purity'] = geyser['purity'][3:]
+            res.update(geyser['location'])
+            nodes_list.append(res)
+
+        for satellite in resources['frackingSatellites']:
+            res = {}
+            res['name'] = satellite['item']['name'].lower().replace(' ', '-') + \
+                '-satellite'
+            res['purity'] = satellite['purity'][3:]
+            res.update(satellite['location'])
+            nodes_list.append(res)
+
+        nodes_df = pandas.DataFrame(nodes_list)
+        del nodes_list
+        nodes_df.to_csv(self.filepath, index=False)
 
     def get_raw(self):
         if ResourceData._raw_data is not None:
             return ResourceData._raw_data
-        self._process_mapdata()
+        if os.path.exists(self.filepath):
+            last_updated = os.path.getmtime(self.filepath)
+            current_time = time.time()
+            if (last_updated - current_time) > 432000:
+                self._process_mapdata()
+        else:
+            self._process_mapdata()
         ResourceData._raw_data = pandas.read_csv(self.filepath)
         return ResourceData._raw_data
